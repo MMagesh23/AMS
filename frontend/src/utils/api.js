d578ef61150fd3955aa9ecc9c59a7b4b25de1abb
Magesh S <91615388+MMagesh23@users.noreply.github.com>
2025-04-23 11:01:29 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // ✅ https://vbs-ams.vercel.app/api
+  baseURL: "https://vbs-sms-api.vercel.app/api", 
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
