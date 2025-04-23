75f31ce9a1c3fab0f322218f4b3a16b2461f997d
Magesh S <91615388+MMagesh23@users.noreply.github.com>
2025-04-23 07:56:33 +0530

    Delete backend directory

--- backend/middleware/roleMiddleware.js
+++
@@ -1,9 +0,0 @@
-const roleMiddleware = (role) => (req, res, next) => {
-    if (req.user.role !== role) {
-      return res.status(403).json({ message: 'Forbidden' });
-    }
-    next();
-  };
-  
-  module.exports = roleMiddleware;
-  
