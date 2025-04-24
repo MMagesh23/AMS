6ba03a0b4515868dd6d2878c4ee6bf1ccc4790ca
Magesh S <91615388+MMagesh23@users.noreply.github.com>
2025-04-24 16:41:21 +0530

    Update Attendance.js

--- backend/models/Attendance.js
+++ backend/models/Attendance.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const attendanceSchema = new mongoose.Schema({
   date: { type: Date, required: true },
   student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student' },
-  status: { type: String, enum: ['Present', 'Absent'], required: true },
-  submittedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Teacher' }
+  status: { type: String, enum: ['present', 'absent'], required: true },
+  submittedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Teacher' },
+  classId: { type: mongoose.Schema.Types.ObjectId, ref: 'Class', required: true }
 });
 
 attendanceSchema.index({ date: 1, student: 1 }, { unique: true });
