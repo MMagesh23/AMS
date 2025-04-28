af0cfab11908c39f6e2201ce2042e6bba930680c
Magesh S <91615388+MMagesh23@users.noreply.github.com>
2025-04-28 16:54:11 +0530

    Update Volunteer.js

--- backend/models/Volunteer.js
+++ backend/models/Volunteer.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const volunteerSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  phone: { type: String, required: true },
+  phone: Number,
 });
 
 module.exports = mongoose.model('Volunteer', volunteerSchema);
